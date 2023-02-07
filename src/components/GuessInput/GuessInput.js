3219563cf8f4cb623c2daa595b688687110bb7b4
jpbougie <jean-philippe@demarque.com>
2023-02-07 16:32:20 -0500

    Exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function GuessInput({ onNewGuess }) {
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (guess.length === 5) {
+          onNewGuess(guess);
+          setGuess("");
+        }
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        minLength={5}
+        maxLength={5}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
